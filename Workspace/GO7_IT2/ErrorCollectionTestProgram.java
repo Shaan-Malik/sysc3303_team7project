80fce2e58d46fbd5529c25b60a6fb8127371e48e
Shaan Malik <shaanmalik@cmail.carleton.ca>
2019-05-22 13:20:56 -0400

    completed ErrorCollectionTestProgram

--- Workspace/GO7_IT2/ErrorCollectionTestProgram.java
+++ Workspace/GO7_IT2/ErrorCollectionTestProgram.java
@@ -6,8 +6,8 @@ public class ErrorCollectionTestProgram {
 		HashMap<Integer, Integer> delayAndSpace = new HashMap<>();
 		
 		Scanner scan = new Scanner(System.in);
-		int type, testModeInt;
-		String filename, datatype, outputMode;
+		int type, packetNumber = 0, timeOrSpace = 0, byteNumber = 0;
+		String packetType;
 		System.out.println("Error Collection Test Program\n");
 		   while (true) {
 			   System.out.print("Enter 0 for normal operation, 1 for loss, 2 for delay, or 3 for duplicate: \n");
@@ -15,40 +15,50 @@ public class ErrorCollectionTestProgram {
 			   if (request.equals("0")) {
 				   type = 0;
 			   }
-			   else if (request.equals("write")) {
+			   else if (request.equals("1")) {
+				   type = 1;
+			   } else if (request.equals("2")) {
 				   type = 2;
+			   } else if (request.equals("3")) {
+				   type = 3;
 			   }
 			   else {
 				   System.out.println("Invalid request type");
 				   continue;
 			   }
-			   System.out.print("Enter filename: \n");
-			   filename = scan.next();
-			   System.out.print("Enter octet or netascii data type: \n");
-			   datatype = scan.next();
-			   if (!((datatype.equals("octet")) || (datatype.equals("netascii")))) {
-				   System.out.println("Invalid data type");
-				   continue;
+			   System.out.print("Enter the request you want to simulate: RRQ, WRQ, ACK, DATA \n");
+			   packetType = scan.next();
+			   if (packetType.equals("ACK") || packetType.equals("DATA")) {
+				   System.out.print("Enter the number of the packet: \n");
+				   packetNumber = Integer.parseInt(scan.next());
 			   }
-			   System.out.print("Enter quiet or verbose mode: \n");
-			   outputMode = scan.next();
-			   if (!((outputMode.equals("quiet")) || (outputMode.equals("verbose")))){
-				   System.out.println("Invalid mode");
-				   continue;
+			   if (type > 1) {
+			   if (type == 2) {
+				   System.out.print("Enter the delay time in seconds: \n");
+			   } else if (type == 3) {
+				   System.out.print("Enter the space between duplicates: \n");
 			   }
-			   System.out.print("Enter normal or test mode: \n");
-			   String testModeString = scan.next();
-			   if (testModeString.equals("normal")) {
-				   testModeInt = 0;
+			   timeOrSpace = Integer.parseInt(scan.next());
 			   }
-			   else if (testModeString.equals("test")) {
-				   testModeInt = 1;
+			   if (packetType.equals("RRQ")) {
+				   byteNumber = Integer.parseInt("0100");
+			   } else if (packetType.equals("WRQ")) {
+				   byteNumber = Integer.parseInt("0200");
+			   } else if (packetType.equals("ACK")) {
+				   byte b1 = (byte)(packetNumber/256);
+				   byte b2 = (byte)(packetNumber%256);
+				   byteNumber = Integer.parseInt("04" + Byte.toString(b1) + Byte.toString(b2));
+			   } else if (packetType.equals("DATA")) {
+				   byte b1 = (byte)(packetNumber/256);
+				   byte b2 = (byte)(packetNumber%256);
+				   byteNumber = Integer.parseInt("03" + Byte.toString(b1) + Byte.toString(b2));
 			   }
-			   else {
-				   System.out.println("Invalid request type");
-				   continue;
-			   }
+			   errors.put(byteNumber, type);
+			   delayAndSpace.put(byteNumber, timeOrSpace);
+			   
+			   System.out.println(byteNumber);
 			   System.out.println("\nRequest initialisation successful\n");
+			   scan.close();
 			   break;
 		   }
 
